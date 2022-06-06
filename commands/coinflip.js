6a1d4388eaa8b1281498e0735c2a56b0062a22c9
Lim Han Hwang <hanhwanglim@gmail.com>
2022-06-06 23:37:55 +0100

    Added more functionality to coinflip.js > Added option to add number of flips

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,10 +3,24 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('flip')
-		.setDescription('Flip a coin!'),
+		.setDescription('Flip a coin!')
+		.addIntegerOption(flips => flips.setName('flips')
+																		.setDescription('Number of flips (Max. 50)')
+																		.setRequired(false)),
 	async execute(interaction) {
-    const coin = Math.floor(Math.random() * 2);
-    if (coin) return interaction.reply('HEADS!');
-    else return interaction.reply('TAILS!');
+		let flips = interaction.options.getInteger('flips');
+
+		if (flips == null) flips = 1
+		if (flips > 50) flips = 50;
+
+		let coins = "";
+		for (let i = 0; i < flips; i++) {
+			const coin = Math.floor(Math.random() * 2);
+			if (coin) coins += "HEADS, "
+			else coins += "TAILS, "
+		}
+
+		coins = coins.slice(0, -2);
+		return interaction.reply(coins);
 	},
 };
