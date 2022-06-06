bdf3a382239e758a1721438fca16f4690126a08f
Lim Han Hwang <hanhwanglim@gmail.com>
2022-06-06 21:11:58 +0100

    Command handling - Moved commands into a command folder > /ping -> commands/ping.js > /server -> commands/server.js > /user -> commands/user.js
    - Changed index.js and deploy-commands.js to read from commands folder

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const dotenv = require('dotenv');
 
 dotenv.config();
-DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 CLIENT_ID = process.env.CLIENT_ID;
 GUILD_ID = process.env.GUILD_ID;
+DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = []
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
 
