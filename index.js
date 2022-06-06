56b35b887ae4c99d592099f63b10704cbe66d795
Lim Han Hwang <hanhwanglim@gmail.com>
2022-06-06 20:05:04 +0100

    Initial setup for Kanou Bot - created ESLint config file - created initial files for bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client( { intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => console.log('Ready'));
+
+client.login(process.env.DISCORD_TOKEN);
