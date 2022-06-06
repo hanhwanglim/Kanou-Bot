7c4cef42ca355d523d0dda1c0df66722396d2a5b
Lim Han Hwang <hanhwanglim@gmail.com>
2022-06-06 20:15:37 +0100

    Creating initial commands > deploy-commands.js > /ping > /server > / user

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ const client = new Client( { intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => console.log('Ready'));
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(process.env.DISCORD_TOKEN);
